ac1f078a35d16bc6bb5b47bbef23ebc867feb857
weirain <wzrzt@hotmail.com>
2017-06-23 22:07:08 +0800

    mask password

--- Rconnect_with_mysql.R
+++ Rconnect_with_mysql.R
@@ -3,7 +3,6 @@
 # Down JDBC driver for mysql from above website. And put it somewhere in disk.
 library(rJava) 
 library(RJDBC)
-# mydb = dbConnect(user='bj_data', password='5H35ZHQkOXQCt11', dbname='dmall_ware', host='s3306i-maria-1.jxq.db.dmall.com')
 
 drv.mysql <- JDBC("com.mysql.jdbc.Driver",
             "/home/runrunwei/JDBC/mysql-connector-java-5.1.42/mysql-connector-java-5.1.42-bin.jar",
